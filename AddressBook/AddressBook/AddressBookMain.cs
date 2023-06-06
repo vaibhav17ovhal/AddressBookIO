73dc0509c75a139986e4a8ff9f47a91e8d67659a
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-06 15:44:05 +0530

    [Vaibhav] Add:Creating Contact

---
+++ AddressBook/AddressBook/AddressBookMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookMain
+    {
+        Contact contact = new Contact();
+        List<Contact> main = new List<Contact>();
+    }
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book I/O");
+        }
+    }
+}
