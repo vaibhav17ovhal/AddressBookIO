49fc43d186dcd52716e9a032dec64b5c0000a4f4
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-06 15:48:33 +0530

    [Vaibhav] Add:Adding new contact

--- AddressBook/AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBook/AddressBookMain.cs
@@ -10,5 +10,38 @@ namespace AddressBook
     {
         Contact contact = new Contact();
         List<Contact> main = new List<Contact>();
+
+        public void Addcontact()
+        {
+            Console.Write("Enter the first name: ");
+            contact.FirstName = Console.ReadLine();
+
+            Console.Write("Enter the last name: ");
+            contact.LastName = Console.ReadLine();
+
+            Console.Write("Enter the address: ");
+            contact.Address = Console.ReadLine();
+
+            Console.Write("Enter the city: ");
+            contact.City = Console.ReadLine();
+
+            Console.Write("Enter the state: ");
+            contact.State = Console.ReadLine();
+
+            Console.Write("Enter the Zip: ");
+            contact.Zip = Console.ReadLine();
+
+            Console.Write("Enter the phone number: ");
+            contact.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter the email: ");
+            contact.Email = Console.ReadLine();
+
+            main.Add(contact);
+        }
+        public void Display()
+        {
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+        }
     }
 }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -7,6 +7,11 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book I/O");
+
+            AddressBookMain book = new AddressBookMain();
+            book.Addcontact();
+            Console.WriteLine("\n");
+            book.Display();
         }
     }
 }
